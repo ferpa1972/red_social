ea26be954975d02232dd5fab003250739106684b
matiasvj <verdier.matias@gmail.com>
2014-05-17 18:16:07 -0300

    controlador de MPost

---
+++ grails-app/controllers/red_social/MPostController.groovy
@@ -0,0 +1,24 @@
+package red_social
+
+class MPostController {
+
+    def listar() {
+        
+    }
+    
+    def mostrar() {
+        
+    }
+    
+    def crear(){
+        
+    }
+    
+    def editar(){
+        
+    }
+    
+    def borrar(){
+        
+    }
+}
