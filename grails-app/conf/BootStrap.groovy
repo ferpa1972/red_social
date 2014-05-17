f142b9ac96f91ee665bf44ef3e2c66a6e8d47cf3
matiasvj <verdier.matias@gmail.com>
2014-05-17 18:10:05 -0300

    crear un usuario de prueba para testear la funcion de login y logout

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,12 @@
+import red_social.Usuario
+
 class BootStrap {
 
     def init = { servletContext ->
+        if (!Usuario.count()){
+            new Usuario(nick:"matiasvj", nombre:"Matias", apellido:"Verdier", email:"verdier.matias@gmail.com", 
+                password:"123456789", fnac:new Date(), pais:"Uruguay", ciudad:"Paysandu", activo:true).save(failOnError:true)
+        }
     }
     def destroy = {
     }
