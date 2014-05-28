db7fa752777f7796fd592cff1a4bd7255f030ab9
matiasvj <verdier.matias@gmail.com>
2014-05-28 19:57:56 -0300

    resolviendo conflictos merge dev y matiasvj

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,7 +5,13 @@ class BootStrap {
     def init = { servletContext ->
         if (!Usuario.count()){
             new Usuario(nick:"matiasvj", nombre:"Matias", apellido:"Verdier", email:"verdier.matias@gmail.com", 
-                password:"123456789", fnac:new Date('1991/03/16'), pais:"Uruguay", ciudad:"Paysandu", activo:true).save(failOnError:true)
+                password:"123", fnac:new Date('1991/03/16'), pais:"Uruguay", ciudad:"Paysandu", activo:true).save(failOnError:true)
+			
+			new Usuario(nick:"alejox", nombre:"Alexandro", apellido:"Roca", email:"alejox21@gmail.com",
+				password:"456", fnac:new Date('1992/05/20'), pais:"Uruguay", ciudad:"Paysandu", activo:true).save(failOnError:true)
+			
+			new Usuario(nick:"matiaswid", nombre:"Matias", apellido:"Widmaier", email:"matiasw@gmail.com",
+				password:"789", fnac:new Date('1992/10/24'), pais:"Uruguay", ciudad:"Paysandu", activo:true).save(failOnError:true)
         }
     }
     def destroy = {
--- grails-app/controllers/red_social/UsuarioController.groovy
+++ grails-app/controllers/red_social/UsuarioController.groovy
@@ -1,97 +1,97 @@
 package red_social
 
 class UsuarioController {
-    
-    def index(){
-        redirect (action:'perfil')
-    }
-    
-    def login(){
-        if (request.get) {
-            return
-        }
-        def usuario = Usuario.findByNick(params.nick)
-        
-        if (usuario) {
-            if (usuario.password == params.password) {
-                session.usuario_logueado = usuario.nick
-                redirect (controller:"MPost", action:"nuevo")
-            }
-            else {
-                render(view: "login", model: [error: "La contraseña no es correcta"])
-            }
-        }
-        else {
-            render(view: "login", model: [error: "El usuario no existe"])
-        }
-    }
-    
-    def logout(){
-        session.invalidate()
-        redirect(action: "login")
-    }
 
-    
-    def perfil() {
-//        	System.out.println "Perfil de "+session.usuario_logueado.toString()
-              // render "Bienvendo "+ session.usuario_logueado.toString()
-        def usu = Usuario.findByNick(session.usuario_logueado.toString())
-        if(usu){
-            [datosUsu:usu]
-        }
-        else{
-            render "Error"
-        }
-    }
-    
-     def editarPerfil() {
-        this.perfil()
-    }
-    
-    
-    def registro(){
-		
-        
-    }
-    
-    def borrar(){
-        def usu = Usuario.findByNick(session.usuario_logueado.toString())
-		
-        usu.activo = false
-        usu.save()
-    }
-	
-    def buscar(){
-        render 'no implementado todavia'
-    }
+	def index(){
+		redirect (action:'perfil')
+	}
 
-    def editarP(){
-        
-        def u = Usuario.findByNick(session.usuario_logueado.toString())
-        if(u){
-            u.nombre=params.nombre
-            u.apellido=params.apellido
-            
-            u.fnac=new Date(params.fnac)
-            u.email=params.email
-            u.pais=params.pais
-            u.ciudad=params.ciudad
-            if( u.save()) {          
-                redirect (action:'perfil')
-            }
-            else{
-                render u.errors
-            }
-            
-        }
-        
-        
-        
-        
-        
-        
-        
-        
-        def usuario= new Usuario(nombre: params.nombre, apellido: params.apellido, fnac:params.fnac, email: params.email, ciudad: params.ciudad, pais: params.pais )
-    }
+	def login(){
+		if (request.get) {
+			return
+		}
+		def usuario = Usuario.findByNick(params.nick)
+
+		if (usuario) {
+			if (usuario.password == params.password) {
+				session.usuario_logueado = usuario.nick
+				redirect (controller:"MPost", action:"nuevo")
+			}
+			else {
+				render(view: "login", model: [error: "La contraseña no es correcta"])
+			}
+		}
+		else {
+			render(view: "login", model: [error: "El usuario no existe"])
+		}
+	}
+
+	def logout(){
+		session.invalidate()
+		redirect(action: "login")
+	}
+
+
+	def perfil() {
+		def usu = Usuario.findByNick(session.usuario_logueado.toString())
+		if(usu){
+			[datosUsu:usu]
+		}
+		else{
+			render "Error"
+		}
+	}
+
+	def editarPerfil() {
+		this.perfil()
+	}
+
+
+	def registro(){
+	}
+
+	def borrar(){
+		def usu = Usuario.findByNick(session.usuario_logueado.toString())
+
+		usu.activo = false
+		usu.save()
+	}
+
+	def editarP(){
+
+		def u = Usuario.findByNick(session.usuario_logueado.toString())
+		if(u){
+			u.nombre=params.nombre
+			u.apellido=params.apellido
+
+			u.fnac=new Date(params.fnac)
+			u.email=params.email
+			u.pais=params.pais
+			u.ciudad=params.ciudad
+			if( u.save()) {
+				redirect (action:'perfil')
+			}
+			else{
+				render u.errors
+			}
+		}
+	}
+
+	def buscar(){
+
+		def busqueda = params.busqueda
+
+		def coincidencias_usuarios = Usuario.where {
+			nombre =~ "%"+busqueda+"%" || nick =~ "%"+busqueda+"%" || apellido =~ "%"+busqueda+"%" || pais =~ "%"+busqueda+"%" || ciudad =~ "%"+busqueda+"%"
+		}
+
+		def coincidencias_posts = MPost.where {
+			contenido =~ "%"+busqueda+"%"
+		}
+
+		def resultado = [usuarios_encontrados:coincidencias_usuarios.list(), posts_encontrados:coincidencias_posts.list()]
+
+
+		[resultado:resultado]
+	}
 }
