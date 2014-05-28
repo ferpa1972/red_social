4c85292c9975e2ca1c20e12178705f0c0c5417b7
matiaswid <diegowidmaier2008@hotmail.com>
2014-05-27 23:41:07 -0300

    followers

--- grails-app/controllers/red_social/UsuarioController.groovy
+++ grails-app/controllers/red_social/UsuarioController.groovy
@@ -1,53 +1,64 @@
 package red_social
 
 class UsuarioController {
-    
-    def index(){
-        redirect (action:'perfil')
-    }
-    
-    def login(){
-        if (request.get) {
-            return
-        }
-        def usuario = Usuario.findByNick(params.nick)
-        
-        if (usuario) {
-            if (usuario.password == params.password) {
-                session.usuario_logueado = usuario.nick
-                redirect (controller:"MPost", action:"nuevo")
-            }
-            else {
-                render(view: "login", model: [error: "La contraseÃ±a no es correcta"])
-            }
-        }
-        else {
-            render(view: "login", model: [error: "El usuario no existe"])
-        }
-    }
-    
-    def logout(){
-        session.invalidate()
-        redirect(action: "login")
-    }
-    
-    def perfil() {
+
+	def index(){
+		redirect (action:'perfil')
+	}
+
+	def login(){
+		if (request.get) {
+			return
+		}
+		def usuario = Usuario.findByNick(params.nick)
+
+		if (usuario) {
+			if (usuario.password == params.password) {
+				session.usuario_logueado = usuario.nick
+				redirect (controller:"MPost", action:"nuevo")
+			}
+			else {
+				render(view: "login", model: [error: "La contraseña no es correcta"])
+			}
+		}
+		else {
+			render(view: "login", model: [error: "El usuario no existe"])
+		}
+	}
+
+	def logout(){
+		session.invalidate()
+		redirect(action: "login")
+	}
+
+	def perfil() {
 		System.out.println "Perfil de "+session.usuario_logueado.toString()
-        render "Bienvendo "+ session.usuario_logueado.toString()
-    }
-    
-    def registro(){
-		
-        
-    }
-    
-    def borrar(){
-        def usu = Usuario.findByNick(session.usuario_logueado.toString())
-		
+		render "Bienvendo "+ session.usuario_logueado.toString()
+	}
+
+	def registro(){
+	}
+
+	def seguir(){
+		def usuarioSeguir = Usuario.findByNick(params.nick)
+		def usuarioActual = Usuario.findbyNick(session.usuario.logueado.toString())
+		if(usuarioSeguri){
+			usuarioSeguir.addToSeguidores(usuarioActual)
+			usuarioSeguir.save()
+		}
+		if(usuarioActual){
+			usuarioActual.addToUsuarios_seguidos(usuarioSeguir)
+			usuarioActual.save()
+		}
+	}
+
+	def borrar(){
+		def usu = Usuario.findByNick(session.usuario_logueado.toString())
+
 		usu.activo = false
 		usu.save()
-    }
-	
+	}
+
 	def buscar(){
 		render 'no implementado todavia'
 	}
